12aec7ca31628342c0415719be3e410d8ebe105d
banxi1988 <banxi1988@gmail.com>
2017-08-04 15:57:12 +0800

    feat 添加UIGestureRecognizerExtensions 为 Gesture.state 增加名字 computed 属性

---
+++ BXiOSUtils/UIGestureRecognizerExtensions.swift
@@ -0,0 +1,22 @@
+//
+//  UIGestureRecognizerExtensions.swift
+//  BXAppKit
+//
+//  Created by Haizhen Lee on 04/08/2017.
+//  Copyright © 2017 banxi1988. All rights reserved.
+//
+
+import UIKit
+
+public extension UIGestureRecognizerState{
+  public var name:String{
+    switch self {
+    case .possible: return "possible"
+    case .began: return "began"
+    case .failed: return "failed"
+    case .cancelled: return "cancelled"
+    case .changed: return "changed"
+    case .ended: return "ended"
+    }
+  }
+}
