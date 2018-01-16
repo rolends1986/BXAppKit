02ba41d0f350e1c972aff704d2a736c9be59ee7e
banxi1988 <banxi1988@gmail.com>
2018-01-16 10:43:57 +0800

    fix 调整默认的字体大小

--- BXForm/Config/FormMetrics.swift
+++ BXForm/Config/FormMetrics.swift
@@ -12,9 +12,9 @@ public struct FormMetrics{
   public static var cornerRadius : CGFloat = 4.0
   public static var iconPadding : CGFloat = 8.0
   public static var designBaseWidth :CGFloat = 375 // 一般以iPhone 6 来设计，其宽度为 750
-  public static var primaryFontSize : CGFloat = 15
-  public static var secondaryFontSize : CGFloat = 13
-  public static var tertiaryFontSize : CGFloat = 11
+  public static var primaryFontSize : CGFloat = 14
+  public static var secondaryFontSize : CGFloat = 12
+  public static var tertiaryFontSize : CGFloat = 10
   public static var cellPaddingLeft: CGFloat = 7
   public static var cellPaddingRight: CGFloat = 7
   public static var cellLabelWidth: CGFloat = 68
